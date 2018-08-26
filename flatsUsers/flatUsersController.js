2c8c917156659885fb7a1ff4be07e038d78195a9
prabakargr <prabakar1993@gmail.com>
2018-08-26 16:31:08 +0530

    res

--- flatsUsers/flatUsersController.js
+++ flatsUsers/flatUsersController.js
@@ -2,9 +2,9 @@ var flatsUsersModel = require('./flatUsersModel');
 
 var createProfile = function(req, res) {
     var flatuser = new flatsUsersModel(req.body);
-    flatuser.save(function(err) {
+    flatuser.save(function(err, result) {
         if (err) return res.status(400).send('cannot create user');
-        else return res.status(200).send('user created');
+        else return res.status(200).send(result);
     });
 }
 
