93b0dc6224e90e749a9c9c00c8204ff5782a5c79
prabakargr <prabakar1993@gmail.com>
2018-08-28 00:22:54 +0530

    changes users

--- flatsUsers/flatUsersController.js
+++ flatsUsers/flatUsersController.js
@@ -2,6 +2,7 @@ var flatsUsersModel = require('./flatUsersModel');
 
 var createProfile = function(req, res) {
     var flatuser = new flatsUsersModel(req.body);
+    console.log(flatuser);
     flatuser.save(function(err, result) {
         if (err) return res.status(400).send('cannot create user');
         else return res.status(200).send(result);
@@ -9,7 +10,7 @@ var createProfile = function(req, res) {
 }
 var findProfile=function(req,res){
     var uid=req.body.uid;
-    flatsUsersModel.findById(uid,function(err,result){
+    flatsUsersModel.findOne({uid},function(err,result){
        if(err) return res.send('connot find');
        else return res.send(result);
     })
