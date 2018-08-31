70141a96d6029250c38eff1c5f9fe56848005d68
prabakargr <prabakar1993@gmail.com>
2018-08-31 14:58:43 +0530

    news update get

--- newsUpdates/newsUpdateRouting.js
+++ newsUpdates/newsUpdateRouting.js
@@ -5,6 +5,7 @@ var newsUpdateRouting = express.Router();
 var newsController = require('./newsUpdatesController');
 
 newsUpdateRouting.route('/createNews').post(newsController.createNews);
+newsUpdateRouting.route('/getAllNews').get(newsController.getAllNews);
 
 
 module.exports = newsUpdateRouting;
--- newsUpdates/newsUpdatesController.js
+++ newsUpdates/newsUpdatesController.js
@@ -8,6 +8,14 @@ var createNews = function(req, res) {
     })
 }
 
+var getAllNews = function(req, res) {
+    newUpdateModel.find(function(err, result) {
+        if (err) res.send('cannot find news');
+        else res.send(result);
+    })
+}
+
 module.exports = {
     createNews: createNews,
+    getAllNews: getAllNews
 }
