4ef35cdfda53c3c6f01db7316771177506eec0f7
prabakargr <prabakar1993@gmail.com>
2018-09-04 14:19:54 +0530

    admin

--- adminUsers/adminUsersController.js
+++ adminUsers/adminUsersController.js
@@ -4,7 +4,7 @@ var createAdminUser = function(req, res) {
     var adminUser = new adminUsersModel(req.body);
     adminUser.save(function(err, result) {
         if (err) res.send('cannot create user');
-        else res.send('user created')
+        else res.send(result)
     })
 };
 
