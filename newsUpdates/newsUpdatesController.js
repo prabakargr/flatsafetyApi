33fdc0e54d2a8ffb54e580b8f82442e60c0d6ef6
prabakargr <prabakar1993@gmail.com>
2018-09-04 17:24:23 +0530

    new update

--- newsUpdates/newsUpdatesController.js
+++ newsUpdates/newsUpdatesController.js
@@ -4,7 +4,7 @@ var createNews = function(req, res) {
     newsupdate = new newUpdateModel(req.body);
     newsupdate.save(function(err, result) {
         if (err) res.send('cannot create');
-        else res.send('news created');
+        else res.send(result);
     })
 }
 
